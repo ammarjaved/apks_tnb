477e37b242f6a218621ff9ff274ac7ba0b11708d
progmaster001 <a.rehman5110@gmail.com>
2024-06-28 00:21:21 +0500

    update

--- app/Http/Controllers/web/LinkBoxMapController.php
+++ app/Http/Controllers/web/LinkBoxMapController.php
@@ -83,9 +83,19 @@ class LinkBoxMapController extends Controller
     public function seacrhCoordinated($lang , $name)
     {
         $name = urldecode($name);
-        $data = LinkBox::where('id' ,$name )->select('id', \DB::raw('ST_X(geom) as x'),\DB::raw('ST_Y(geom) as y'),)->first();
+        $data = LinkBox::where('id' ,$name )
+                        ->pluck('geom_id')
+                        ->first();
 
-        return response()->json($data, 200);
+
+        $geom = \DB::table('tbl_link_box_geom')
+                    ->where('id',$data)
+                    ->select(
+                        \DB::raw('ST_X(geom) as x'),
+                        \DB::raw('ST_Y(geom) as y')
+                    )->first();
+
+        return response()->json($geom, 200);
     }
 
 }
